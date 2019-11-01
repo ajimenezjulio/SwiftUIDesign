3ffc6826e908a4422ca86b3a4375db53ed01a8c2
Aguilar, Julio <Julio.Aguilar@meijer.com>
2019-11-01 13:58:24 -0400

    Modal Presentation

--- SwiftUIDesign/HomeList.swift
+++ SwiftUIDesign/HomeList.swift
@@ -9,13 +9,27 @@
 import SwiftUI
 
 struct HomeList: View {
+    var courses = coursesData
+    @State var showContent = false
+    
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            HStack {
-                ForEach(0 ..< 3) { item in
-                    CourseView()
+        ScrollView(Axis.Set.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(courses) { item in
+                    
+                    Button(action: {
+                        self.showContent.toggle()
+                    }, label: {
+                        CourseView(title: item.title,
+                                   image: item.image,
+                                   color: item.color,
+                                   shadowColor: item.shadowColor)
+                        .sheet(isPresented: self.$showContent) { ContentView() }
+                    })
                 }
             }
+            .padding(.leading, 30)
+            Spacer()
         }
     }
 }
@@ -30,21 +44,58 @@ struct HomeList_Previews: PreviewProvider {
 // MARK: - View Components
 
 struct CourseView : View {
+    var title = "Build an app with SwiftUI"
+    var image = "Illustration1"
+    var color = Color("background3")
+    var shadowColor = Color("backgroundShadow3")
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Build an app with SwiftUI")
+        return VStack(alignment: .leading) {
+            Text(title)
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(.white)
                 .padding(20)
                 .lineLimit(4)
-                .frame(width: 150)
+                // Make consistent the size of the text element for all texts
+                .padding(.trailing, 50)
             Spacer()
-            Image("Illustration1")
+            Image(image)
+                .resizable()
+                // Avoid the issue that the image has the color of the tint
+                .renderingMode(.original)
+                // Adjustin images to the size
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 246, height: 150)
+                // Make consistent the sizes of the images by adding a padding
+                .padding(.bottom, 30)
         }
-        .background(Color("background3"))
+        .background(color)
         .cornerRadius(30)
         .frame(width: 246, height: 360)
-        .shadow(color: Color("backgroundShadow3"), radius: 20, x: 0, y: 20)
+        .shadow(color: shadowColor, radius: 20, x: 0, y: 20)
     }
 }
+
+
+// MARK: - Data Model
+
+struct Course : Identifiable {
+    var id = UUID()
+    var title : String
+    var image : String
+    var color : Color
+    var shadowColor : Color
+}
+
+let coursesData = [
+    Course(title: "Build an app with SwiftUI",
+           image: "Illustration1",
+           color: Color("background3"),
+           shadowColor: Color("backgroundShadow3")),
+    
+    Course(title: "Design Course",
+           image: "Illustration2",
+           color: Color("background4"),
+           shadowColor: Color("backgroundShadow4"))
+]
